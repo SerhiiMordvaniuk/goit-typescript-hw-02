3534d488af935b42372a9ad2f2d7fcf4e3dadc1c
Serhii Mordvaniuk <sm636332@gmail.com>
2025-02-14 18:34:04 +0100

    add TS for data in API

--- src/gallery-api.ts
+++ src/gallery-api.ts
@@ -1,4 +1,13 @@
 import axios from "axios";
+import { Image } from "./App.types";
+
+type FetchData = {
+  data: {
+    total: number;
+    total_page: number;
+    results: Image[];
+  };
+};
 
 const KEY = "1X6RII3RoeScRHg5if82ucbsdRGfgCmSu4y1o-_zRdE";
 
@@ -6,10 +15,12 @@ const fetchSearch = async (search: string, page: number) => {
   if (search.trim() === "") {
     return;
   }
-  const response = await axios.get(
+  const { data }: FetchData = await axios.get(
     `https://api.unsplash.com/search/photos/?page=${page}&per_page=12&client_id=${KEY}&query=${search}`
   );
-  return response.data;
+  console.log(data);
+
+  return data;
 };
 
 export default fetchSearch;
